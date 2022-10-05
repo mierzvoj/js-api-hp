0f99a0a9ffb3e46d32b4b037b71e352375086e35
mierzvoj <mierzvoj@gmail.com>
2022-10-05 21:17:41 +0200

    working on objects in data presentation layer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,51 @@
-const fetchAllStudents = fetch("https://hp-api.herokuapp.com/api/characters");
-fetchAllStudents
-  .then((response) => {
-    return response.json();
-  })
-  .then((allStudents) => {
-    const crucialData = allStudents
-      .map((allStudents) => allStudents.name)
-      .join("\n");
-    console.log(crucialData);
+// const fetchAllStudents = fetch("https://hp-api.herokuapp.com/api/characters");
+// fetchAllStudents
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((allStudents) => {
+//     console.log(allStudents);
+//   });
+
+const showAllStudentsBtn = document.getElementById("show_all");
+const students = [];
+const renderStudents = () => {
+  const fetchAllPromise = fetch("https://hp-api.herokuapp.com/api/characters");
+  fetchAllPromise
+    .then((response) => {
+      return response.json();
+    })
+    .then((allStudents) => {
+      const names = allStudents.map((student) => student.name).join;
+    });
+
+  const studentsList = document.getElementById("students_list");
+  if (students.length === 0) {
+    studentsList.classList.remove("visible");
+    return;
+  } else {
+    studentsList.classList.add("visible");
+  }
+  studentsList.innerHTML = "";
+  students.forEach((student) => {
+    const studentEl = document.createElement("li");
+    let text = student.name;
+    studentEl.textContent = text;
+    studentsList.append(studentEl);
   });
+};
+const addAllStudentsHandler = () => {
+  const newStudent = {
+    name,
+    dateOfBirth,
+    house,
+    wizard,
+    ancestry,
+    hogwartsStaff,
+    hogwartsStudent,
+  };
+  students.push(newStudent);
+  renderStudents();
+};
+
+showAllStudentsBtn.addEventListener("click", addAllStudentsHandler);
