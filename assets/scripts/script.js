589a2366fb3a9fe8f4cc9fa281ff727fc30e983a
mierzvoj <mierzvoj@gmail.com>
2022-10-07 20:27:24 +0200

    some content rendered in a table

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,10 +6,8 @@ const renderStudents = () => {
     .then((response) => {
       return response.json();
     })
-    .then((allStudents) => {
-      const names = allStudents.map((student) => student.name).join("\n");
-      console.log(names);
-      students.push(names);
+    .then((students) => {
+      const names = students.map((student) => student.name);
       const studentsList = document.getElementById("students_list");
       if (students.length === 0) {
         studentsList.classList.remove("visible");
@@ -17,13 +15,50 @@ const renderStudents = () => {
       } else {
         studentsList.classList.add("visible");
       }
-      studentsList.innerHTML = listOfAllStudents(allStudents);
+      listCreate(studentsList, students);
+      tableCreate(studentsList, students);
     });
 };
-function listOfAllStudents(people) {
-  const names = people.map((person) => `<li>${person.name}</li>`).join("\n");
-  return `<ul>${names}</ul>`;
+function listCreate(container, students) {
+  container.innerHTML = "";
+  students.forEach((student) => {
+    const studentEl = document.createElement("li");
+    let text = student.name;
+    console.log(student.name);
+    studentEl.textContent = text;
+    container.append(studentEl);
+  });
 }
+function tableCreate(container, students) {
+  const tbl = document.createElement("table");
+  tbl.style.width = "100px";
+  tbl.style.border = "1px solid black";
+  const columns = [];
+
+  if (students.length) {
+    const tHeader = tbl.createTHead();
+    const tr = tHeader.insertRow();
+    tr.style.fontWeight = "bold";
+    for (const key of Object.keys(students[0])) {
+      columns.push(key);
+      const td = tr.insertCell();
+      td.appendChild(document.createTextNode(key));
+      td.style.border = "1px solid black";
+    }
+  }
+
+  const tBody = tbl.createTBody();
+  for (let i = 0; i < students.length; i++) {
+    const tr = tBody.insertRow();
+    for (let j = 0; j < columns.length; j++) {
+      const td = tr.insertCell();
+      td.appendChild(document.createTextNode(students[i][columns[j]]));
+      td.style.border = "1px solid black";
+    }
+  }
+  container.appendChild(tbl);
+}
+
 const addAllStudentsHandler = () => {
   renderStudents();
 };
