a756f04a4067feb4f7ade57bda1e7badbe70ab18
mierzvoj <mierzvoj@gmail.com>
2022-10-10 19:38:54 +0200

    content rendered in list, table and modal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -44,20 +44,69 @@ function tableCreate(container, students) {
       const td = tr.insertCell();
       td.appendChild(document.createTextNode(key));
       td.style.border = "1px solid black";
+      td.style.cursor = "pointer";
+      td.addEventListener("click", () => {
+        const desc = td.classList.contains("asc") ? true : false;
+        if (desc) {
+          td.classList.remove("asc");
+          td.classList.add("desc");
+        }
+        if (!desc) {
+          td.classList.remove("desc");
+          td.classList.add("asc");
+        }
+        createTableBodyContent(tBody, columns, students, key, desc);
+      });
     }
   }
 
   const tBody = tbl.createTBody();
-  for (let i = 0; i < students.length; i++) {
+  createTableBodyContent(tBody, columns, students);
+  container.appendChild(tbl);
+}
+function createTableBodyContent(tBody, columns, students, sortColumn, desc) {
+  tBody.innerHTML = "";
+  let result = students.slice();
+  if (sortColumn) {
+    const sign = desc ? -1 : 1;
+    result.sort((a, b) => (a[sortColumn] > b[sortColumn] ? 1 : -1) * sign);
+  }
+  for (let i = 0; i < result.length; i++) {
     const tr = tBody.insertRow();
+    tr.addEventListener("dblclick", () => openModal(result[i]));
     for (let j = 0; j < columns.length; j++) {
       const td = tr.insertCell();
-      td.appendChild(document.createTextNode(students[i][columns[j]]));
+      td.appendChild(document.createTextNode(result[i][columns[j]]));
       td.style.border = "1px solid black";
     }
   }
-  container.appendChild(tbl);
 }
+
+function openModal(student) {
+  const modal = document.getElementById("myModal");
+  const content = document.getElementById("myModalContent");
+  content.innerHTML = "";
+  for (const key of Object.keys(student)) {
+    const span = document.createElement("span");
+    const br = document.createElement("br");
+    span.innerText = `[${key}]: ${student[key]}`;
+    content.appendChild(span);
+    content.appendChild(br);
+  }
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  const modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  const modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    closeModal();
+  }
+};
 
 const addAllStudentsHandler = () => {
   renderStudents();
